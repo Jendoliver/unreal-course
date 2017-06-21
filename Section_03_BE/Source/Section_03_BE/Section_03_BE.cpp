f5cb2db9c6a387150ff81e319d1d564a3326003e
Jendoliver <alvarezmonllor@gmail.com>
2017-06-21 03:22:33 +0200

    .gitignore and project structure refactored

---
+++ Section_03_BE/Source/Section_03_BE/Section_03_BE.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Section_03_BE.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Section_03_BE, "Section_03_BE" );
---
+++ Section_03_BE/Source/Section_03_BE/Section_03_BEGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "Section_03_BEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECTION_03_BE_API ASection_03_BEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
